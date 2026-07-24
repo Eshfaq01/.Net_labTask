602d6fb7a46d168b2ad2ed009299e73038a75925
Eshfaq01 <eshfaqjahanchhoya123@gmail.com>
2026-07-24 22:34:05 +0600

    ok

---
+++ assignment/EF/BloodBankDbContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using assignment.EF.Tables;
+
+namespace assignment.EF;
+
+public partial class BloodBankDbContext : DbContext
+{
+    public BloodBankDbContext()
+    {
+    }
+
+    public BloodBankDbContext(DbContextOptions<BloodBankDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Donation> Donations { get; set; }
+
+    public virtual DbSet<Donor> Donors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=DbConn");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Donation>(entity =>
+        {
+            entity.ToTable("Donation");
+
+            entity.Property(e => e.CampName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Donor).WithMany(p => p.Donations)
+                .HasForeignKey(d => d.DonorId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Donation_Donor");
+        });
+
+        modelBuilder.Entity<Donor>(entity =>
+        {
+            entity.ToTable("Donor");
+
+            entity.Property(e => e.BloodGroup)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.City)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.ContactNo)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.FullName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
