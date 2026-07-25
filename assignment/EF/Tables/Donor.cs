b8f257b9641939b94ec7f6e623dbc2040f28fc06
Eshfaq01 <eshfaqjahanchhoya123@gmail.com>
2026-07-25 21:56:28 +0600

    validation

--- assignment/EF/Tables/Donor.cs
+++ assignment/EF/Tables/Donor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace assignment.EF.Tables;
 
@@ -7,12 +8,17 @@ public partial class Donor
 {
     public int DonorId { get; set; }
 
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Name must be between 3 to 100")]
     public string FullName { get; set; } = null!;
 
+    [Required]
     public string BloodGroup { get; set; } = null!;
 
+    [Required]
     public string ContactNo { get; set; } = null!;
 
+    [Required]
     public string City { get; set; } = null!;
 
     public DateOnly LastDonationDate { get; set; }
